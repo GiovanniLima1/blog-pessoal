72ba75befd4b3fd803f5ce4ab48a6c18d3994d8b
Giovanni <giovannilima32@gmail.com>
2024-01-11 12:50:37 -0300

    Criando classe controller, model e Repository

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,69 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="tb_postagens")
+public class Postagem {
+   
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Size(min = 5, max = 100, message = "O título deve ter pelo menos 5 caracteres e no méximo 100")
+	@NotBlank(message = "Atributo título é obrigatório")
+	private String titulo;
+	
+	
+	@Size(min = 10, max = 1000, message = "O texto deve ter pelo menos 10 caracteres e no méximo 1000")
+	@NotBlank(message = "Atributo texto é obrigatório")
+	private String texto;
+	
+	@UpdateTimestamp 
+	private LocalDateTime data;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	} 
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	} 
+	
+	
+}
+ 
